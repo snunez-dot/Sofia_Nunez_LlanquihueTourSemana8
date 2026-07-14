e9d8ff02d951c07ce4bd987671b4a6fb993e444d
snunez-dot <so.nunezg@duocuc.cl>
2026-07-13 23:18:51 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+package cl.llanquihuetour.ui;
+
+import cl.llanquihuetour.data.GestorEntidades;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GestorEntidades gestor = new GestorEntidades();
+
+        gestor.iniciarSistema();
+
+    }
+
+}
